4b0a8affa48faadbb81000d0f90f2c52ae03f35e
Rune Haugaard <noloxs@gmail.com>
2024-04-24 20:31:10 +0200

    Update pyautogui to use keys from settings
    Signed-off-by: Rune Haugaard <noloxs@gmail.com>

--- executer_pyautogui.py
+++ executer_pyautogui.py
@@ -1,26 +1,28 @@
-import time
-import random
 import pyautogui
 from executer_base import BaseExecutor
-from config import strategemTriggerKey, triggerDelayMax, triggerDelayMin
+import utilities
 
 class PyAutoGuiExecuter(BaseExecutor):
     def __init__(self, model):
         super().__init__()
         self.model = model
         pyautogui.FAILSAFE = True
+        self.triggerKey = self.parse_macro_key(self.model.settings.triggerKey)
     
     def on_macro_triggered(self, macro):
         try:
             if macro != None:
-                pyautogui.keyDown(strategemTriggerKey)
-                time.sleep(0.1)
+                pyautogui.keyDown(self.triggerKey)
+                utilities.sleepTriggerKey(self.model)
                 for input in macro.commandArray:
                     pyautogui.keyDown(input)
-                    time.sleep(random.uniform(triggerDelayMin,triggerDelayMax))
+                    utilities.sleepStrategemKey(self.model)
                     pyautogui.keyUp(input)
-                    time.sleep(random.uniform(triggerDelayMin,triggerDelayMax))
-                pyautogui.keyUp(strategemTriggerKey)
+                    utilities.sleepStrategemKey(self.model)
+                pyautogui.keyUp(self.triggerKey)
 
         except AttributeError:
             pass
+    
+    def parse_macro_key(self, key):
+        return key
