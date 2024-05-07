80468773cb95ae0b278f4940cdb3c7c6eadf69c7
Rune Haugaard <noloxs@gmail.com>
2024-05-07 20:30:39 +0200

    Initial test for executing in a thread
    Signed-off-by: Rune Haugaard <noloxs@gmail.com>

--- src/executer_pynput.py
+++ src/executer_pynput.py
@@ -1,6 +1,7 @@
 from pynput.keyboard import Controller
 from src.executer_base import BaseExecutor
 from src import utilities, key_parser_pynput
+import threading
 
 class PynputExecuter(BaseExecutor):
     def __init__(self, model):
@@ -12,17 +13,21 @@ class PynputExecuter(BaseExecutor):
     def on_macro_triggered(self, macro):
         try:
             if macro != None:
-                self.keyboard_controller.press(self.triggerKey)
-                utilities.sleepTriggerKey(self.model)
-                for input in macro.commandArray:
-                    self.keyboard_controller.press(input)
-                    utilities.sleepStrategemKey(self.model)
-                    self.keyboard_controller.release(input)
-                    utilities.sleepStrategemKey(self.model)
-                self.keyboard_controller.release(self.triggerKey)
+                t1 = threading.Thread(target=self.execute_macro, args=(macro,))
+                t1.start()
 
         except AttributeError:
             pass
     
+    def execute_macro(self, macro):
+        self.keyboard_controller.press(self.triggerKey)
+        utilities.sleepTriggerKey(self.model)
+        for input in macro.commandArray:
+            self.keyboard_controller.press(input)
+            utilities.sleepStrategemKey(self.model)
+            self.keyboard_controller.release(input)
+            utilities.sleepStrategemKey(self.model)
+        self.keyboard_controller.release(self.triggerKey)
+
     def parse_macro_key(self, key):
         return key_parser_pynput.parse_key(key)
